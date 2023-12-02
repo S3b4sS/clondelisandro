9cc1065f55daabcc94b65ec126b4883920974348
ssuf <ssuf2002@gmail.com>
2023-12-02 10:48:50 -0400

    detail view

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Article
 # Create your views here idiot.
@@ -7,3 +7,7 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
