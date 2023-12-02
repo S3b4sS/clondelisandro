4032070a9c39c47509fd698c052792ce978241d3
ssuf <ssuf2002@gmail.com>
2023-12-02 11:33:58 -0400

    article edit

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView
+from .views import (ArticleListView, ArticleDetailView, ArticleUpdateView)
 
 urlpatterns =[
     path('', ArticleListView.as_view(), name='article_list'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('edit/<int:pk>/', ArticleUpdateView.as_view(), name='article_edit'),
 ]
